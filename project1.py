2b9c63fc7f1a11a67948a5f305567d76c156c50b
RossVerploegh <rverploe@alumni.nd.edu>
2017-12-24 13:12:57 -0800

    project 1 code tested

--- project1.py
+++ project1.py
@@ -1,5 +1,21 @@
+#!/usr/bin/python
 
-import numpy
 
-#Project 1 from Euler
-#Ross Verploegh
+def Mutiplesof3and5(number):
+    """Finds the sum of all the multiples of 3 or 5 below a given number.
+
+    Args:
+        number - Sets the range of natural numbers to search, exclusive.
+    Returns:
+        Integer sum of the all multiples of 3 or 5 below the stated number.
+    """
+    summation = int(0)
+    for n in range(1,number,1):
+        if n%3 == 0 or n%5 == 0:
+            summation += n
+    return summation	
+
+
+if __name__ == '__main__':
+  
+    print(Mutiplesof3and5(1000))
