b1dab83163aea599fa6fe0de3bdb58988dc61315
Ross James Verploegh <rossjamesverploegh@Rosss-MBP.numat.local>
2018-01-16 11:21:30 -0600

    flake8 passed

--- project1.py
+++ project1.py
@@ -10,12 +10,11 @@ def Mutiplesof3and5(number):
         Integer sum of the all multiples of 3 or 5 below the stated number.
     """
     summation = int(0)
-    for n in range(1,number,1):
-        if n%3 == 0 or n%5 == 0:
+    for n in range(1, number, 1):
+        if n % 3 == 0 or n % 5 == 0:
             summation += n
-    return summation	
+    return summation
 
 
 if __name__ == '__main__':
-  
     print(Mutiplesof3and5(1000))
