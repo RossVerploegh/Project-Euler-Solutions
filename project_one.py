6fc73b61bade3de6d3c86652f30197b24673551f
Patrick Fuller <patrickfuller@gmail.com>
2018-01-17 11:23:15 -0600

    PEP8-ified function name

--- project_one.py
+++ project_one.py
@@ -1,4 +1,4 @@
-def mutiplesof3and5(number):
+def mutiples_of_3_and_5(number):
     """Finds the sum of all the multiples of 3 or 5 below a given number.
 
     Args:
@@ -11,4 +11,4 @@ def mutiplesof3and5(number):
 
 
 if __name__ == '__main__':
-    print(mutiplesof3and5(1000))
+    print(mutiples_of_3_and_5(1000))
